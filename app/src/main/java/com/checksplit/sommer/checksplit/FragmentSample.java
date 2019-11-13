43c98762489ca9e04e9c9849557a2cedb8efc257
patricksommer <ptscreations@gmail.com>
2019-11-12 21:02:42 -0500

    Setup sample for fragment in activity

---
+++ app/src/main/java/com/checksplit/sommer/checksplit/FragmentSample.java
@@ -0,0 +1,37 @@
+package com.checksplit.sommer.checksplit;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.checksplit.sommer.checksplit.databinding.FragmentSampleBinding;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+public class FragmentSample extends Fragment {
+
+    private FragmentSampleBinding fragmentSampleBinding;
+
+    public FragmentSample() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        fragmentSampleBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_sample,null,false);
+
+        return fragmentSampleBinding.getRoot();
+    }
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    public FragmentSampleBinding getFragmentSampleBinding() {
+        return fragmentSampleBinding;
+    }
+}
--- app/src/main/java/com/checksplit/sommer/checksplit/MainActivity.java
+++ app/src/main/java/com/checksplit/sommer/checksplit/MainActivity.java
@@ -2,22 +2,39 @@ package com.checksplit.sommer.checksplit;
 
 
 import android.os.Bundle;
-import com.checksplit.sommer.checksplit.databinding.ActivityMainBindingImpl;
+
+import com.checksplit.sommer.checksplit.databinding.ActivityMainBinding;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentSample fragmentSampleIncluded;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ActivityMainBindingImpl activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-
+        // Set up binding for MainActivity
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         activityMainBinding.setLifecycleOwner(this);
 
+        // Create Text Model. Set it as a variable in MainActivity Binding
         TextModel textModel = new TextModel();
-
         activityMainBinding.setVariable(BR.model, textModel);
+
+        // Create a fragment by code.
+        // Inside 'activity_main.xml' there is an FrameLayout with id 'fragmentContainer' tag.
+        // This fragment will replace that object
+        fragmentSampleIncluded = new FragmentSample();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragmentSampleIncluded).commit();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Inside 'activity_main' there is an <include> tag with the id 'fragmentSampleIncluded'
+        // This is an included layout that has no code behind it. We can access its elements with binding
+        fragmentSampleIncluded.getFragmentSampleBinding().label.setText("Fragment From Include");
     }
 }
